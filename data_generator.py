d732166ef2f5e3eed5ee619d36337255b3531e67
Vinoj John Hosan <vinojhosan@gmail.com>
2019-05-29 20:10:17 +0530

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,5 +1,5 @@
 from keras.utils import Sequence
-from config import IMAGE_H, IMAGE_W, GRID_H, GRID_W, BATCH_SIZE, BOX, ANCHORS, TRUE_BOX_BUFFER, LABELS
+from config import IMAGE_H, IMAGE_W, GRID_H, GRID_W, BATCH_SIZE, BOX, ANCHORS, TRUE_BOX_BUFFER, LABELS, CLASS
 import cv2 as cv
 import numpy as np
 import numpy.random as random
@@ -60,71 +60,6 @@ class BatchGenerator(Sequence):
             iaa.Fliplr(0.5),  # horizontally flip 50% of all images
             iaa.Flipud(0.2),
         ])
-
-        # ### augmentors by https://github.com/aleju/imgaug
-        # sometimes = lambda aug: iaa.Sometimes(0.5, aug)
-        #
-        # # Define our sequence of augmentation steps that will be applied to every image
-        # # All augmenters with per_channel=0.5 will sample one value _per image_
-        # # in 50% of all cases. In all other cases they will sample new values
-        # # _per channel_.
-        # self.aug_pipe = iaa.Sequential(
-        #     [
-        #         # apply the following augmenters to most images
-        #         # iaa.Fliplr(0.5), # horizontally flip 50% of all images
-        #         # iaa.Flipud(0.2), # vertically flip 20% of all images
-        #         # sometimes(iaa.Crop(percent=(0, 0.1))), # crop images by 0-10% of their height/width
-        #         sometimes(iaa.Affine(
-        #             # scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
-        #             # translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-        #             # rotate=(-5, 5), # rotate by -45 to +45 degrees
-        #             # shear=(-5, 5), # shear by -16 to +16 degrees
-        #             # order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
-        #             # cval=(0, 255), # if mode is constant, use a cval between 0 and 255
-        #             # mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
-        #         )),
-        #         # execute 0 to 5 of the following (less important) augmenters per image
-        #         # don't execute all of them, as that would often be way too strong
-        #         iaa.SomeOf((0, 5),
-        #                    [
-        #                        # sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
-        #                        iaa.OneOf([
-        #                            iaa.GaussianBlur((0, 3.0)),  # blur images with a sigma between 0 and 3.0
-        #                            iaa.AverageBlur(k=(2, 7)),
-        #                            # blur image using local means with kernel sizes between 2 and 7
-        #                            iaa.MedianBlur(k=(3, 11)),
-        #                            # blur image using local medians with kernel sizes between 2 and 7
-        #                        ]),
-        #                        iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen images
-        #                        # iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
-        #                        # search either for all edges or for directed edges
-        #                        # sometimes(iaa.OneOf([
-        #                        #    iaa.EdgeDetect(alpha=(0, 0.7)),
-        #                        #    iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
-        #                        # ])),
-        #                        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5),
-        #                        # add gaussian noise to images
-        #                        iaa.OneOf([
-        #                            iaa.Dropout((0.01, 0.1), per_channel=0.5),  # randomly remove up to 10% of the pixels
-        #                            # iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
-        #                        ]),
-        #                        # iaa.Invert(0.05, per_channel=True), # invert color channels
-        #                        iaa.Add((-10, 10), per_channel=0.5),
-        #                        # change brightness of images (by -10 to 10 of original value)
-        #                        iaa.Multiply((0.5, 1.5), per_channel=0.5),
-        #                        # change brightness of images (50-150% of original value)
-        #                        iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
-        #                        # iaa.Grayscale(alpha=(0.0, 1.0)),
-        #                        # sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
-        #                        # sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))) # sometimes move parts of the image around
-        #                    ],
-        #                    random_order=True
-        #                    )
-        #     ],
-        #     random_order=True
-        # )
-        #
-        # if shuffle: np.random.shuffle(self.images)
 
     def file_2_data_structure(self, image_path, csv_path):
 
@@ -243,7 +178,10 @@ class BatchGenerator(Sequence):
 
         x_batch = np.zeros((BATCH_SIZE, IMAGE_H, IMAGE_W, 3))  # input images
         b_batch = np.zeros((BATCH_SIZE, 1, 1, 1, TRUE_BOX_BUFFER, 4))  # GT boxes
-        y_batch = np.zeros((BATCH_SIZE, GRID_H, GRID_W, BOX, 4 + 1 + len(LABELS)))  # desired network output
+        y_batch = np.zeros((BATCH_SIZE, GRID_H, GRID_W, BOX, 4 + 1 + CLASS + 1))  # desired network output
+
+        # confidence_for_non_class
+        y_batch[..., -1] = 1
 
         for t in range(l_bound,r_bound):
             # augment input image and fix object's position and size
@@ -299,6 +237,7 @@ class BatchGenerator(Sequence):
                     y_batch[instance_count, grid_y, grid_x, best_anchor, 0:4] = box
                     y_batch[instance_count, grid_y, grid_x, best_anchor, 4] = 1.
                     y_batch[instance_count, grid_y, grid_x, best_anchor, 5 + obj_index] = 1
+                    y_batch[instance_count, grid_y, grid_x, best_anchor, -1] = 0
 
                     # assign the true box to b_batch
                     b_batch[instance_count, 0, 0, 0, true_box_index] = box
@@ -315,7 +254,7 @@ class BatchGenerator(Sequence):
 
             # print(' new batch created', idx)
 
-        return [x_batch, b_batch], y_batch
+        return x_batch, y_batch
 
     def on_epoch_end(self):
         if self.shuffle: np.random.shuffle(self.image_list)

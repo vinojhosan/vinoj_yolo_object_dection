727d2f25933d5ecf3da9dc4273dcfa2d77327911
Vinoj John Hosan <vinojhosan@gmail.com>
2019-06-26 15:20:03 +0530

    Create ssd_model.py

---
+++ ssd_model.py
@@ -0,0 +1,68 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
+
+import tensorflow as tf
+import keras as k
+import synthetic_dataset as dataset
+import ssd_loss
+
+input_h = 300
+input_w = 300
+
+grid_h = 10
+grid_w = 10
+batch_size = 8
+
+ds = dataset.SynthenticGenerator((input_h, input_w), (grid_h, grid_w), batch_size=8, n_images=1000)
+
+
+def get_model():
+    input_layer = k.Input((input_h, input_w, 3), name='input_image')
+
+    resnet50 = k.applications.ResNet50(include_top=False, input_tensor=input_layer, input_shape=(input_h, input_w, 3), weights='imagenet', pooling=None) #(input_layer)
+    # resnet50.summary()
+    # print(resnet50.shape)
+    activation_49 = resnet50.get_layer('activation_49').output
+
+    output_channel = len(ds.anchors)*(ds.n_class+1 + 4)
+    resnet_connector = k.layers.Conv2D(output_channel, (3, 3), padding='same', activation='relu')(activation_49)
+    print(resnet_connector.shape)
+    reshaping = k.layers.Reshape([grid_h, grid_w, len(ds.anchors), (ds.n_class + 1 + 4)])(resnet_connector)
+
+    ssd_model = k.models.Model(input_layer, reshaping)
+
+    ssd_model.summary()
+
+    return ssd_model
+
+def identity_metric(y_true, y_pred):
+    return k.backend.mean(y_pred)
+
+def train():
+
+    yolo_model = get_model()
+    # yolo_model.load_weights('models/trained_model_32x32_20kp_batdataset.hdf5')
+
+    adam = k.optimizers.Adam(lr=0.005)
+
+    yolo_model.compile(adam, loss=ssd_loss.ssd_loss, metrics=[identity_metric])
+
+    # Model saving and checkpoint callbacks
+    yolo_model.save('models/empty_model_10x10.hdf5')
+    filepath = "models/weights.best_pose_10x10.hdf5"
+    checkpoint = k.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False,
+                                                 mode='min')
+    tensorboard = k.callbacks.TensorBoard(log_dir="./logs", write_graph=True, write_images=True)
+    callbacks_list = [checkpoint, tensorboard]
+
+    # pose_aug_generator - Augments and reads images.  Obtains num_keypoints+rgb channels
+    yolo_model.fit_generator(ds,
+                             steps_per_epoch=ds.n_images // batch_size,
+                             epochs=1000,
+                             callbacks=callbacks_list)
+
+    yolo_model.save('models/trained_model_10x10.hdf5')
+
+
+train()
